2b01bb239403cf5f6b3c000d2c9582fe58d14abe
EVERTON DIOGO FERREIRA MELO <everton_d_melo@estudante.sesisenai.org.br>
2026-02-26 22:00:26 -0300

    aulas

---
+++ exemplos/numeros-operadores.js
@@ -0,0 +1,28 @@
+function numerosOperadores() {
+    const idade = 50;
+    const gols = 1000;
+    const pi = 3.14; //decimal
+
+const expoente = 3 ** 2; // 9 "Ao Quadrado"
+const modulo = 15 % 2; // 1"Resto da divisão"
+
+const soma = '100' + 50; // 10050
+const subtracao = '100' - 50; // 50
+const multiplicacao = '100' * '2'; // 200
+const divisao = 'Blabla 100' / 2; // NaN (Not a Number)
+
+const numero = 70;
+const unidade = 'kg';
+const peso = numero + unidade; // '80kg'
+const pesoPorDois = peso / 2; // NaN (Not a Number)
+
+const incremento = 5;
+console.log(incremento++); // 5
+console.log(incremento); // 6
+
+const incremento2 = 5;
+console.log(++incremento2); // 6
+console.log(incremento2); // 6
+
+}
+
