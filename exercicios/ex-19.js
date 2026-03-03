ad0cd62d2dc5254f3499d3451e50fd6919c1de3b
EVERTON DIOGO FERREIRA MELO <everton_d_melo@estudante.sesisenai.org.br>
2026-03-02 21:31:13 -0300

    Exercícios finalizados

--- exercicios/ex-19.js
+++ exercicios/ex-19.js
@@ -1,14 +1,14 @@
 const numeros = [18, 7, 15, 19, 53, 12, 40, 30, 34, 66];
 
-const resultado = numeros.map(num => {
-    if (num % 3 === 0 && num % 5 === 0) {
+const resultado = numeros.map(numero => {
+    if (numero % 3 === 0 && numero % 5 === 0) {
         return "FizzBuzz";
     } else if (num % 3 === 0) {
         return "Fizz";
     } else if (num % 5 === 0) {
         return "Buzz";
     } else {
-        return num;
+        return numero;
     }
 });
 
